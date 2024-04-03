45e9da5999df46cff53ee136f32a94700eaeac59
David Martinez E <david.martinez.e@usach.cl>
2024-04-03 21:20:01 +0200

    Delete eva1 directory

--- eva1/src/main/java/tingesoV1/eva1/repositories/IngresoARepRepository.java
+++
@@ -1,7 +0,0 @@
-package tingesoV1.eva1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tingesoV1.eva1.entities.IngresoARepEntity;
-
-public interface IngresoARepRepository extends JpaRepository<IngresoARepEntity,Long> {
-}
--- eva1/src/main/java/tingesoV1/eva1/repositories/VehiculoRepository.java
+++
@@ -1,8 +0,0 @@
-package tingesoV1.eva1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tingesoV1.eva1.entities.VehiculoEntity;
-
-public interface VehiculoRepository extends JpaRepository<VehiculoEntity,Long> {
-
-}
