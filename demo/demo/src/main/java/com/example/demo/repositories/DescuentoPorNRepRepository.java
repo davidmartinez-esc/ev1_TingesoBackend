4cc51fca63495313c324b029c7224474300e5747
Poke Martinez <david.martinez.e@usach.cl>
2024-04-26 16:35:56 +0200

    Borrada carpeta de demo residual

--- demo/demo/src/main/java/com/example/demo/repositories/DescuentoPorNRepRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.DescuentoPorNRepEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-//import tingesoV1.eva1.entities.DescuentoPorNRepEntity;
-@Repository
-public interface DescuentoPorNRepRepository extends JpaRepository<DescuentoPorNRepEntity,Long> {
-}
--- demo/demo/src/main/java/com/example/demo/repositories/RecargoAplicadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.RecargoAplicadoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-//import tingesoV1.eva1.entities.RecargoAplicadoEntity;
-@Repository
-public interface RecargoAplicadoRepository extends JpaRepository<RecargoAplicadoEntity,Long> {
-}
