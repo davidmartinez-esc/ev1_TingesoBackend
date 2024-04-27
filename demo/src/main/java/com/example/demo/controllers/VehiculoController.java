50bad4d86ae903fd1f611e730ec9d8155d63cfe4
Poke Martinez <david.martinez.e@usach.cl>
2024-04-28 00:55:26 +0200

    Savestate previo a cambios

--- demo/src/main/java/com/example/demo/controllers/VehiculoController.java
+++ demo/src/main/java/com/example/demo/controllers/VehiculoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/vehiculo")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class VehiculoController {
     @Autowired
     VehiculoService vehiculoService;
